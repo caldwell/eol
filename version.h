3f5169beed1337b63c7b1c8e9dfc29d946531622
David Caldwell <david@indigita.com>
2001-09-21 00:18:26 +0000

    - new version number for release

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
-#define MajorVersion 1
-#define MinorVersion 0
-#define DevelVersion 0
+#define MajorVersion  1
+#define MinorVersion  0
+#define BugFixVersion 1
 
 #define Cat2(x,y) x##y
 #define Cat(x,y) Cat2(x,y)
 #define MajorVersionBCD Cat(0x,MajorVersion)
 #define MinorVersionBCD Cat(0x,MinorVersion)
-#define DevelVersionBCD Cat(0x,DevelVersion)
+#define BugFixVersionBCD Cat(0x,BugFixVersion)
 
 #define Quote(x) #x
 #define XYZQuoteConCat(x,y,z) Quote(x) "." Quote(y) "." Quote(z)
-#define VersionString XYZQuoteConCat(MajorVersion,MinorVersion,DevelVersion)
+#define VersionString XYZQuoteConCat(MajorVersion,MinorVersion,BugFixVersion)
 
 #ifdef VERSION_STRING_NOW
 main () { printf(VersionString); }
