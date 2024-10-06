9916388fafff7af8ff73522506b6238ca8168fee
David Caldwell <david@porkrind.org>
2024-10-06 15:19:39 -0700

    debian/: Remove a bunch of unneeded examples

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the eol package
-#
-0 4	* * *	root	eol_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(eol):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="eol" command="/usr/bin/eol"
